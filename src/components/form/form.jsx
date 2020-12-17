68bf29cc1c6bbfca4a1663e12cf2ba6fb0f00525
sho1374k <sho4771k@gmail.com>
2020-12-17 16:53:00 +0900

    style追加

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -2,36 +2,39 @@ import React from "react";
 import {Btn} from "../button/btn";
 
 export const Form = (props) => {
+  const height = window.innerHeight;
+
   return(
     <>
-    { props.openTodo === true &&
-    <form className="form" onSubmit={props.addTodo} >
-      <div className="form-title">
-        <input className="form-title_input" type="text" name="title" placeholder="enter title"/>
-        { props.error.errorTitle === true &&
-          // エラー
-          <div className="form-title_error">
-            タイトルを入力してください
+      { props.openTodo === true &&
+      <div className="modal" style={{height: height}}>
+        <form className="form" onSubmit={props.addTodo} >
+          <div className="form-title">
+            <input className="form-title_input" type="text" name="title" placeholder="enter title"/>
+            { props.error.errorTitle === true &&
+              // エラー
+              <div className="form-title_error">
+                タイトルを入力してください
+              </div>
+            }
           </div>
-        }
-      </div>
-      
-      <div className="form-content">
-        <textarea className="form-content_input" name="content" id="" cols="30" rows="4" placeholder="enter text"></textarea>
-        {props.error.errorContent === true &&
-          // エラー
-          <div className="form-content__error">
-            テキストを入力してください
+          
+          <div className="form-content">
+            <textarea className="form-content_input" name="content" id="" cols="30" rows="4" placeholder="enter text"></textarea>
+            {props.error.errorContent === true &&
+              // エラー
+              <div className="form-content_error">
+                テキストを入力してください
+              </div>
+            }
           </div>
-        }
+          <div className="form-btns">
+            <button className="btn btn-add-todo" type="submit">add todo</button>
+            <Btn  name={"Close"} styleName={"btn btn-close-form"} actionClick={props.handleTodo} value={String} i={String} />
+          </div>
+        </form>
       </div>
-
-      <div className="form-btns">
-        <button className="btn btn-add-todo" type="submit">add todo</button>
-        <Btn  name={"閉じる"} styleName={"btn btn-close-form"} actionClick={props.handleTodo} value={String} i={String} />
-      </div>
-    </form>
-    }
+      }
     </>
   )
 }
--- src/components/todo/doing.jsx
+++ src/components/todo/doing.jsx
@@ -6,41 +6,43 @@ import {Btn} from "../button/btn";
 export const Doing = (props) => {
   return(
     <div className="doing">
-      <h1 className="doing-title">Doing List</h1>
+      <h2 className="doing-title">Doing List</h2>
       {props.data.map((todo, i) => {
         let number = todo.id
         return(
-          <div className="doing-content" key={i}>
+          <>
           { todo.edit === true && todo.doing === true ?
-            <EditForm 
-              editTodo={props.editTodo}
-              editTitle={props.editTitle}
-              editContent={props.editContent}
-              editError={props.editError}
-              changeEdit={props.changeEdit}
-              number={number}
-              todo={todo}
-              i={i}
-            />
+            <div className="doing-content" key={i}>
+              <EditForm 
+                editTodo={props.editTodo}
+                editTitle={props.editTitle}
+                editContent={props.editContent}
+                editError={props.editError}
+                changeEdit={props.changeEdit}
+                number={number}
+                todo={todo}
+                i={i}
+              />
+            </div>
           :
           <>
             {todo.doing === true &&
-            <>
+            <div className="doing-content" key={i}>
               <div className="doing-content-des" key={i}>
                 <IndividualTodo 
                   todo={todo}
                 />
               </div>
               <div className="doing-content-btns">
-                <Btn  name={"確認待ちへ"} styleName={"btn btn-review"} actionClick={props.changeReview} value={number} i={String} />
-                <Btn  name={"編集する"} styleName={"btn btn-edit-todo"} actionClick={props.changeEdit} value={number} i={String} />
+                <Btn  name={"Next"} styleName={"btn btn-next"} actionClick={props.changeReview} value={number} i={String} />
+                <Btn  name={"Edit"} styleName={"btn btn-edit-todo"} actionClick={props.changeEdit} value={number} i={String} />
                 <br/><br/>
               </div>
-            </>
+            </div>
             }
           </>
           }
-        </div>
+          </>
         )
       })}
     </div>
